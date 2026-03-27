8f64666bf560634674997a7f051fe5e325d11fc6
dazzes4545 <dazzes7619@gmail.com>
2026-03-27 10:32:33 +0300

    Step 5: refactoring code for calculator

--- calculator/main.js
+++ calculator/main.js
@@ -1,27 +1,33 @@
-const display = document.getElementById("display");
-const buttons = document.querySelectorAll("button");
-const clearBtn = document.getElementById("clear");
-let expression = "";
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const value = button.textContent;
-    if (value === "C") {
-      expression = "";
-      display.value = "";
-      return;
-    } 
-    
-    if (value === "=") {
-        try {
-            expression = eval(expression);
-            display.value = expression;
-        } catch {
-            display.value = "Ошибка";
-            expression = "";
-        }
-        return
-    }
-    expression += value;
-    display.value = expression; 
-  });
+import { Calculator } from "./calculator.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const calc = new Calculator();
+  calc.init();
 });
+// const display = document.getElementById("display");
+// const buttons = document.querySelectorAll("button");
+// const clearBtn = document.getElementById("clear");
+// let expression = "";
+// buttons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     const value = button.textContent;
+//     if (value === "C") {
+//       expression = "";
+//       display.value = "";
+//       return;
+//     }
+
+//     if (value === "=") {
+//         try {
+//             expression = eval(expression);
+//             display.value = expression;
+//         } catch {
+//             display.value = "Ошибка";
+//             expression = "";
+//         }
+//         return
+//     }
+//     expression += value;
+//     display.value = expression;
+//   });
+// });
